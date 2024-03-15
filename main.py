d7985b835ed6534e25c44dfde9f5350dff0b381e
obafemicoker <femiecoker@gmail.com>
2024-03-15 10:46:18 +0000

    App largely working, just need to tweak the returned URL

--- main.py
+++ main.py
@@ -1,38 +1,62 @@
 import streamlit as st
 import pandas as pd
 
-utm_sources = ["eloqua","email", "adwords", "ads","facebok", "twitter","instagram","linkedin", "bing","google","duckduckgo", "eloqua"]
-utm_mediums = ["social","socialmedia","social-media","display", "email","paid-social","paid_social","ppc-social","search","cpc","ppc","paid"]
-st.title("URL Builder with UTM Parameters")
-st.subheader("blah blah blah blah")
+utm_sources = ['eloqua','email', 'adwords', 'ads','facebok', 'twitter','instagram','linkedin', 'bing','google','duckduckgo', 'eloqua']
+utm_mediums = ['social','socialmedia','social-media','display', 'email','paid-social','paid_social','ppc-social','search','cpc','ppc','paid']
 
-st.text_input("Enter Base URL", key = "base_url")
-st.subheader("Select your source and medium")
-col1, col2 = st.columns(2)
-with col1:
-    st.multiselect("Source", options = utm_sources, max_selections=1)
+def link_creation(base_url, utm_source, utm_medium, utm_campaign, utm_content, utm_term, utm_id, utm_ref):
+    utm_params = []
+    if utm_source:
+        utm_params.append(f"utm_source={utm_sources}")
+    if utm_medium:
+        utm_params.append(f"utm_medium={utm_mediums}")
+    if utm_campaign:
+        utm_params.append(f"utm_campaign={utm_campaign}")
+    if utm_content:
+        utm_params.append(f"utm_content={utm_content}")
+    if utm_term:
+        utm_params.append(f"utm_term={utm_term}")
+    if utm_ref:
+        utm_params.append(f"utm_ref={utm_ref}")
+    if utm_id:
+        utm_params.append(f"utm_id={utm_id}")
 
-with col2:
-    st.multiselect("Medium", options= utm_mediums, max_selections=1)
+    utm_params_str = "&".join(utm_params)
+    return f"{base_url}?{utm_params_str}"
 
-st.subheader("Select your additional UTM parameters")
-col3, col4 = st.columns(2)
-with col3:
-    st.checkbox(label = "Campaign", value = False, key = "campaign_param")
-    st.checkbox(label = "Term", value = False, key = "term_param")
-    st.checkbox(label = "Content", value = False, key = "content_param")
-    st.checkbox(label = "Ref", value = False, key = "ref_param")
-    st.checkbox(label = "id", value = False, key = "id")
-with col4:
-    st.text_input("Enter Campaign Identifier", key = "campaign_input")
-    st.text_input("Enter Term Identifier", key = "term_input")
-    st.text_input("Enter Content Identifier", key = "content_input")
-    st.text_input("Enter Ref Identifier", key = "ref_input")
-    st.text_input("Enter ID Identifier", key = "id_input")
+def main():
+    st.title("URL Builder with UTM Parameters")
+    st.subheader("blah blah blah blah")
 
-st.button("Create Link", key = "runit", on_click=None)
+    with st.form("utm_creator",clear_on_submit=False):
+        base_url = st.text_input("Enter Base URL", key = "base_url")
+        st.subheader("Select your source and medium")
+        col1, col2 = st.columns(2)
+        with col1:
+            utm_source = st.multiselect("Source", options = utm_sources, max_selections=1)
 
-st.text_area("Final URL", key= "final_url")
+        with col2:
+            utm_medium = st.multiselect("Medium", options= utm_mediums, max_selections=1)
 
-def link_creation(base_url):
-    pass
+        st.subheader("Select your additional UTM parameters")
+        utm_campaign = st.text_input("Enter Campaign Identifier", key = "campaign_input")
+        utm_term = st.text_input("Enter Term Identifier", key = "term_input")
+        utm_content = st.text_input("Enter Content Identifier", key = "content_input")
+        utm_ref = st.text_input("Enter Ref Identifier", key = "ref_input")
+        utm_id = st.text_input("Enter ID Identifier", key = "id_input")
+
+        submitted = st.form_submit_button('Build URL')
+
+        if submitted and base_url:
+            utm_url = link_creation(base_url, utm_source, utm_medium, utm_campaign, utm_term, utm_content, utm_ref, utm_id)
+            st.success('Your UTM URL is:')
+            st.code(utm_url)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
