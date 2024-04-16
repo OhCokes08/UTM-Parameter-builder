53717d0cee561ab6608dffe8d41a3ff3e73eba42
obafemicoker <femiecoker@gmail.com>
2024-04-16 09:49:20 +0100

    Link saving to CSV functionality

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime as dt
+import csv
 import streamlit as st
 import pandas as pd
+
 
 utm_sources = ["eloqua","email", "adwords", "ads","facebook", "twitter","instagram","linkedin", "bing","google","duckduckgo", "eloqua"]
 utm_mediums = ["social","socialmedia","social-media","display", "email","paid-social","paid_social","ppc-social","search","cpc","ppc","paid"]
@@ -24,18 +27,30 @@ def link_creation(base_url, utm_source, utm_medium, utm_campaign, utm_content, u
     utm_params_str = "&".join(utm_params)
     return f"{base_url}/?{utm_params_str}"
 
+def link_save(url, doc = "tracking_links.csv"):
+    time = dt.datetime.now()
+    date = time.strftime("%d-%m-%Y")
+    link = url
+    with open(doc, mode = 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([date,link])
+
 def main():
-    st.title("URL Builder with UTM Parameters")
-    st.subheader("blah blah blah blah")
+    st.image('https://static.vecteezy.com/system/resources/previews/000/176/200/original/vector-abstract-company-logo-template-design-illustration.jpg',use_column_width= True)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.title("RX URL Builder with UTM Parameters")
+    with col2:
+        st.subheader("blah blah blah blah")
 
     with st.form("utm_creator",clear_on_submit=False):
         base_url = st.text_input("Enter Base URL", key = "base_url")
         st.subheader("Select your source and medium")
-        col1, col2 = st.columns(2)
-        with col1:
+        col3, col4 = st.columns(2)
+        with col3:
             utm_source = st.multiselect("Source", options = utm_sources, max_selections=1)
 
-        with col2:
+        with col4:
             utm_medium = st.multiselect("Medium", options= utm_mediums, max_selections=1)
 
         st.subheader("Select your additional UTM parameters")
@@ -52,6 +67,7 @@ def main():
             st.success('Your UTM URL is:')
             print(utm_url)
             st.code(utm_url)
+            link_save(utm_url)
 
 if __name__ == "__main__":
     main()
